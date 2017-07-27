2220cb4fb6495e053efba2dac067d762c64b3088
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-27 17:40:25 -0500

    Change script/build.sh to script/smlnj.sh
    Now the functionality provided by script/build.sh is smlnj-specific

--- script/build.sh
+++
@@ -1,1 +0,0 @@
-echo "OS.Process.exit(OS.Process.failure)" | sml -m src/main.cm && printf 'sml @SMLload=sequent.x86-darwin $@\n' > sequent
---
+++ script/smlnj.sh
@@ -0,0 +1,1 @@
+echo "CM.make \"src/diophantine.cm\"; val _ = SMLofNJ.exportFn (\"solve\", Solver.main)" | sml
